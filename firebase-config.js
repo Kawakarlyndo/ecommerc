347fab80eb40551e90a64f8aa7b95118833bd9f4
Kawã karlyndo <160342064+Kawakarlyndo@users.noreply.github.com>
2025-07-08 11:06:23 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Configuração do Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyCGHYT4oC-GVKaFW_6RBHy1zrgd5wihDcM",
-    authDomain: "portfolio-91d7b.firebaseapp.com",
-    databaseURL: "https://portfolio-91d7b-default-rtdb.firebaseio.com",
-    projectId: "portfolio-91d7b",
-    storageBucket: "portfolio-91d7b.firebasestorage.app",
-    messagingSenderId: "6133951328",
-    appId: "1:6133951328:web:4f5efca1c3deb2ddf50c05",
-    measurementId: "G-FMYLX10FDS"
+  apiKey: "AIzaSyCGHYT4oC-GVKaFW_6RBHy1zrgd5wihDcM",
+  authDomain: "portfolio-91d7b.firebaseapp.com",
+  databaseURL: "https://portfolio-91d7b-default-rtdb.firebaseio.com",
+  projectId: "portfolio-91d7b",
+  storageBucket: "portfolio-91d7b.firebasestorage.app",
+  messagingSenderId: "6133951328",
+  appId: "1:6133951328:web:4f5efca1c3deb2ddf50c05",
+  measurementId: "G-FMYLX10FDS"
 };
 
 // Inicialize o Firebase
